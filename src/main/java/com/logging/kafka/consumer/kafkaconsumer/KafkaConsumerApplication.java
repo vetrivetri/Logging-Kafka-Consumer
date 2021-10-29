be2f9ab2aa7224867b17215f552088aa91a040f7
vetrivetri <51082669+vetrivetri@users.noreply.github.com>
2021-10-29 16:16:56 +0530

    JENKINS BUILD TEST FOR CI

--- src/main/java/com/logging/kafka/consumer/kafkaconsumer/KafkaConsumerApplication.java
+++ src/main/java/com/logging/kafka/consumer/kafkaconsumer/KafkaConsumerApplication.java
@@ -8,7 +8,7 @@ public class KafkaConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaConsumerApplication.class, args);
-		System.out.println("Test Commit For Jenkins CI");
+		System.out.println("Test Commit For Jenkins CI ");
 	}
 
 }
